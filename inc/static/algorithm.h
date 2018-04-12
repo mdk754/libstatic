964eb1ba19750180c58df5911e66017f9090579d
Brian Bos <9956583+mdk754@users.noreply.github.com>
2018-04-12 08:39:13 -0400

    Add various algorithms for swap and exchange

--- inc/static/algorithm.h
+++ inc/static/algorithm.h
@@ -10,6 +10,149 @@
 
 namespace sstd {
 
+/** @brief  Exchanges the given values.
+ *  @tparam T  Type of object to swap.
+ *  @param  a  First object.
+ *  @param  b  Second object.
+ */
+template<class T>
+inline void swap(T& a, T& b) {
+	T tmp(a);
+	a = b;
+	b = tmp;
+}
+
+/** @brief  Swaps the values of the elements the given iterators point to.
+ *  @tparam ForwardIt1  First forward iterator type.
+ *  @tparam ForwardIt2  Second forward iterator type, must dereference to same
+ *                      type as ForwardIt1.
+ *  @param  a  First object.
+ *  @param  b  Second object.
+ */
+template<class ForwardIt1, class ForwardIt2>
+inline void iter_swap(ForwardIt1 a, ForwardIt2 b) {
+	swap(*a, *b);
+}
+
+/** @brief  Exchanges elements between range [first1, last1] and another range
+ *          starting at first2.
+ *  @tparam ForwardIt1  First forward iterator type.
+ *  @tparam ForwardIt2  Second forward iterator type, must dereference to same
+ *                      type as ForwardIt1.
+ *  @param  first1  The begin of the first range to be swapped.
+ *  @param  last1  The end of the first range to be swapped.
+ *  @param  first2  The begin of the second range to be swapped.
+ */
+template<class ForwardIt1, class ForwardIt2>
+inline ForwardIt2 swap_ranges(ForwardIt1 first1,
+                              ForwardIt1 last1,
+                              ForwardIt2 first2) {
+	for (; first1 != last1; ++first1, ++first2) { iter_swap(first1, first2); }
+
+	return first2;
+}
+
+/** @brief  Assigns the given value to the elements in the range [first, last].
+ *  @tparam ForwardIt  Forward iterator type of the range being filled.
+ *  @tparam T  The type of the value to copy into the range.
+ *  @param  first  The begin of the range to assign.
+ *  @param  last  The end of the range to assign.
+ *  @param  val  The value to fill the range with.
+ */
+template<class ForwardIt, class T>
+inline void fill(ForwardIt first, ForwardIt last, const T& val) {
+	for (; first != last; ++first) { *first = val; }
+}
+
+/** @brief  Assigns the given value to the first len elements in the range
+ *          beginning at first if len > 0.
+ *  @tparam OutputIt  Output iterator type of the range being filled.
+ *  @tparam Size  The size type of the elements.
+ *  @tparam T  The type of the value to copy into the range.
+ *  @param  first  The begin of the range to assign.
+ *  @param  len  The number of elements in the range.
+ *  @param  val  The value to fill the range with.
+ */
+template<class OutputIt, class Size, class T>
+inline OutputIt fill_n(OutputIt first, Size len, const T& val) {
+	for (; len; --len, ++first) { *first = val; }
+
+	return first;
+}
+
+/** @brief  Copies the elements in the range, defined by [first, last], to
+ *          another range beginning at dest.
+ *  @tparam InputIt  Input iterator type of the range being copied.
+ *  @tparam OutputIt  Output iterator type of the range being filled.
+ *  @param  first  The begin of the range to copy.
+ *  @param  last  The end of the range to copy.
+ *  @param  dest  The begin of the destination range.
+ */
+template<class InputIt, class OutputIt>
+inline void copy(InputIt first, InputIt last, OutputIt dest) {
+	for (; first != last; ++first, ++dest) { *dest = *first; }
+}
+
+/** @brief  Copies exactly len values from the range beginning at first to the
+ *          range beginning at dest, if len > 0.
+ *  @tparam InputIt  Input iterator type of the range being copied.
+ *  @tparam Size  The size type of the elements.
+ *  @tparam OutputIt  Output iterator type of the range being filled.
+ *  @param  first  The begin of the range to copy.
+ *  @param  len  The number of elements in the range.
+ *  @param  dest  The begin of the destination range.
+ */
+template<class InputIt, class Size, class OutputIt>
+inline OutputIt copy_n(InputIt first, Size len, OutputIt dest) {
+	for (; len; --len, ++first, ++dest) { *dest = *first; }
+
+	return dest;
+}
+
+/** @brief  Returns the smaller of the given values.
+ *  @tparam T  Type of the values to compare.
+ *  @param  a  First value.
+ *  @param  b  Second value.
+ */
+template<class T>
+inline const T& min(const T& a, const T& b) {
+	return b < a ? b : a;
+}
+
+/** @brief  Returns the smaller of the given values, via comparison functor.
+ *  @tparam T  Type of the values to compare.
+ *  @tparam Compare  Binary comparison functor returns true if a < b.
+ *  @param  a  First value.
+ *  @param  b  Second value.
+ *  @param  comp  Comparison functor.
+ */
+template<class T, class Compare>
+inline const T& min(const T& a, const T& b, Compare comp) {
+	return comp(b, a) ? b : a;
+}
+
+/** @brief  Returns the greater of the given values.
+ *  @tparam T  Type of the values to compare.
+ *  @param  a  First value.
+ *  @param  b  Second value.
+ */
+template<class T>
+inline const T& max(const T& a, const T& b) {
+	return a < b ? b : a;
+}
+
+/** @brief  Returns the greater of the given values, via comparison functor.
+ *  @tparam T  Type of the values to compare.
+ *  @tparam Compare  Binary comparison functor returns true if a < b.
+ *  @param  a  First value.
+ *  @param  b  Second value.
+ *  @param  comp  Comparison functor.
+ */
+template<class T, class Compare>
+inline const T& max(const T& a, const T& b, Compare comp) {
+	return comp(a, b) ? b : a;
+}
+
 /** @brief  Returns true if the range [first1, last1] is equal to the range
  *          beginning at first2.
  *  @tparam InputIt1  Must meet the requirements of InputIterator.
@@ -91,6 +234,39 @@ bool lexicographical_compare(InputIt1 first1,
 	}
 
 	return (first1 == last1) && (first2 != last2);
+}
+
+/** @brief  Reverses the order of the elements in the range [first, last].
+ *  @tparam BidirIt  Bidirectional Iterator type of the range.
+ *  @param  first  The begin of the range to be reversed.
+ *  @param  last  The end of the range to be reversed.
+ */
+template<class BidirIt>
+inline void reverse(BidirIt first, BidirIt last) {
+	for (; first != last && first != --last; ++first) {
+		iter_swap(first, last);
+	}
+}
+
+/** @brief  Performs a left rotation on a range of elements.
+ *  @tparam BidirIt  Bidirectional Iterator type of the range.
+ *  @param  first  The begin of the range to be rotated.
+ *  @param  n_first  The position of the new first element after the rotation.
+ *  @param  last  The end of the range to be rotated.
+ *
+ * You can perform a right rotation by using reverse iterators in place of the
+ * normal iterators.
+ */
+template<class BidirIt>
+BidirIt rotate(BidirIt first, BidirIt n_first, BidirIt last) {
+	if (first == n_first) { return last; }
+	if (n_first == last) { return first; }
+
+	reverse(first, n_first);
+	reverse(n_first, last);
+	reverse(first, last);
+
+	return first + distance(n_first, last);
 }
 
 } /* namespace sstd */
--- test/algorithm.cpp
+++ test/algorithm.cpp
@@ -2,6 +2,170 @@
 
 #include <catch/catch.hpp>
 
+struct Foo {
+	int value;
+};
+
+TEST_CASE("Swap two types with one another", "[swap]") {
+	SECTION("Using fundamental type") {
+		int a = 16;
+		int b = 256;
+
+		sstd::swap(a, b);
+
+		REQUIRE(a == 256);
+		REQUIRE(b == 16);
+	}
+
+	SECTION("Using pointer type") {
+		int a = 16;
+		int b = 256;
+
+		int* pa = &a;
+		int* pb = &b;
+
+		sstd::swap(pa, pb);
+
+		REQUIRE(a == 16);
+		REQUIRE(*pa == 256);
+		REQUIRE(b == 256);
+		REQUIRE(*pb == 16);
+	}
+
+	SECTION("Using user-defined type") {
+		Foo a, b;
+
+		a.value = 8;
+		b.value = 128;
+
+		sstd::swap(a, b);
+
+		REQUIRE(a.value == 128);
+		REQUIRE(b.value == 8);
+	}
+}
+
+TEST_CASE("Swap two types via iterators", "[swap]") {
+	SECTION("Single swap via iterator") {
+		int a = 32;
+		int b = 64;
+
+		sstd::iter_swap(&a, &b);
+
+		REQUIRE(a == 64);
+		REQUIRE(b == 32);
+	}
+
+	SECTION("Swap range via iterator") {
+		const size_t count = 4;
+
+		unsigned a[count] = {0, 1, 2, 3};
+		unsigned b[count] = {3, 2, 1, 0};
+
+		sstd::swap_ranges(&a[0], &a[count], &b[0]);
+
+		REQUIRE(a[0] == 3);
+		REQUIRE(a[1] == 2);
+		REQUIRE(a[2] == 1);
+		REQUIRE(a[3] == 0);
+
+		REQUIRE(b[0] == 0);
+		REQUIRE(b[1] == 1);
+		REQUIRE(b[2] == 2);
+		REQUIRE(b[3] == 3);
+	}
+}
+
+TEST_CASE("Fill array with values", "[fill]") {
+	const size_t count = 3;
+	int a[count]       = {0};
+
+	SECTION("Fill range using iterators") {
+		sstd::fill(&a[0], &a[count], 32);
+
+		REQUIRE(a[0] == 32);
+		REQUIRE(a[1] == 32);
+		REQUIRE(a[2] == 32);
+	}
+
+	SECTION("Fill range with count items") {
+		sstd::fill_n(&a[0], count, 64);
+
+		REQUIRE(a[0] == 64);
+		REQUIRE(a[1] == 64);
+		REQUIRE(a[2] == 64);
+	}
+}
+
+TEST_CASE("Copy array to another", "[copy]") {
+	const size_t count = 3;
+	int a[count]       = {2, 2, 2};
+
+	SECTION("Copy range using iterators") {
+		int b[count] = {0};
+
+		sstd::copy(&a[0], &a[count], &b[0]);
+
+		REQUIRE(b[0] == 2);
+		REQUIRE(b[1] == 2);
+		REQUIRE(b[2] == 2);
+	}
+
+	SECTION("Copy range with count items") {
+		int b[count] = {0};
+
+		sstd::copy_n(&a[0], count, b);
+
+		REQUIRE(b[0] == 2);
+		REQUIRE(b[1] == 2);
+		REQUIRE(b[2] == 2);
+	}
+}
+
+TEST_CASE("Check the minimum value", "[comparison]") {
+	int a = 7;
+
+	SECTION("When the values are equal") {
+		int b = 7;
+
+		REQUIRE(sstd::min(a, b) == 7);
+	}
+
+	SECTION("When the first value is lower") {
+		int b = 9;
+
+		REQUIRE(sstd::min(a, b) == 7);
+	}
+
+	SECTION("When the second value is lower") {
+		int b = 3;
+
+		REQUIRE(sstd::min(a, b) == 3);
+	}
+}
+
+TEST_CASE("Check the maximum value", "[comparison]") {
+	int a = 7;
+
+	SECTION("When the values are equal") {
+		int b = 7;
+
+		REQUIRE(sstd::max(a, b) == 7);
+	}
+
+	SECTION("When the first value is lower") {
+		int b = 9;
+
+		REQUIRE(sstd::max(a, b) == 9);
+	}
+
+	SECTION("When the second value is lower") {
+		int b = 3;
+
+		REQUIRE(sstd::max(a, b) == 7);
+	}
+}
+
 TEST_CASE("Check for equality", "[comparison]") {
 	const size_t count = 3;
 	int a[count]       = {0, 1, 2};
@@ -57,5 +221,61 @@ TEST_CASE("Lexicographically compare values", "[comparison]") {
 	SECTION("Two empty ranges are equal") {
 		REQUIRE(!sstd::lexicographical_compare(a, a, &a[3], &a[3]));
 		REQUIRE(!sstd::lexicographical_compare(&a[3], &a[3], a, a));
+	}
+}
+
+TEST_CASE("Reverse an array", "[reverse]") {
+	SECTION("With an even number of elements") {
+		const size_t count = 4;
+		int start[count]   = {0, 1, 2, 3};
+		int finish[count]  = {3, 2, 1, 0};
+
+		sstd::reverse(start, &start[count]);
+
+		REQUIRE(start[0] == finish[0]);
+		REQUIRE(start[1] == finish[1]);
+		REQUIRE(start[2] == finish[2]);
+		REQUIRE(start[3] == finish[3]);
+	}
+
+	SECTION("With an odd number of elements") {
+		const size_t count = 3;
+		int start[count]   = {0, 1, 2};
+		int finish[count]  = {2, 1, 0};
+
+		sstd::reverse(start, &start[count]);
+
+		REQUIRE(start[0] == finish[0]);
+		REQUIRE(start[1] == finish[1]);
+		REQUIRE(start[2] == finish[2]);
+	}
+}
+
+TEST_CASE("Rotate an array", "[rotate]") {
+	const size_t count = 4;
+	int start[count]   = {0, 1, 2, 3};
+
+	SECTION("Left rotate") {
+		int finish[count] = {1, 2, 3, 0};
+
+		sstd::rotate(&start[0], &start[1], &start[count]);
+
+		REQUIRE(start[0] == finish[0]);
+		REQUIRE(start[1] == finish[1]);
+		REQUIRE(start[2] == finish[2]);
+		REQUIRE(start[3] == finish[3]);
+	}
+
+	SECTION("Right rotate") {
+		int finish[count] = {3, 0, 1, 2};
+
+		sstd::rotate(sstd::reverse_iterator<int*>(&start[count]),
+		             sstd::reverse_iterator<int*>(&start[count - 1]),
+		             sstd::reverse_iterator<int*>(&start[0]));
+
+		REQUIRE(start[0] == finish[0]);
+		REQUIRE(start[1] == finish[1]);
+		REQUIRE(start[2] == finish[2]);
+		REQUIRE(start[3] == finish[3]);
 	}
 }
